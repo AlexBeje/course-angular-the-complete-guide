be37a50412b36d1e873228c0f4276f4276051783
Alex <alex.bejenaru93@gmail>
2020-11-19 09:40:30 +0100

    📦 NEW: Added the recipe list and detail components content.

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecepiesModel } from '../recepies.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
-  recipies = [];
+  recipies: RecepiesModel[] = [
+    new RecepiesModel(
+      'A test recipe',
+      'This is simply a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    ),
+    new RecepiesModel(
+      'A test recipe',
+      'This is simply a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    ),
+  ];
 
   constructor() {}
 
