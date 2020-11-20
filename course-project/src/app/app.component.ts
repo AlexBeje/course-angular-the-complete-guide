07f5cd70443f3b20db2837efdbb667c674abc4c2
Alex <alex.bejenaru93@gmail>
2020-11-20 11:22:28 +0100

    📦 NEW: Added event binding to the header (@Output) to create a navigation system in the app component.

--- course-project/src/app/app.component.ts
+++ course-project/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'course-project';
+  loadedFeature = 'recipies';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
