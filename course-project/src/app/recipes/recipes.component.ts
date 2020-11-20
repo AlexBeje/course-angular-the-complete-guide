87b64300d4250083b13e93284e68b4b6124b84e8
Alex <alex.bejenaru93@gmail>
2020-11-20 16:21:30 +0100

    📦 NEW: Passed the data recived by event emitter with propery emitter.

--- course-project/src/app/recipes/recipes.component.ts
+++ course-project/src/app/recipes/recipes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RecepieModel } from './recepie.model';
 
 @Component({
@@ -9,14 +9,16 @@ import { RecepieModel } from './recepie.model';
 export class RecipesComponent {
   recipies: RecepieModel[] = [
     new RecepieModel(
-      'A test recipe',
-      'This is simply a test',
+      'Test recipe 1',
+      'This is simply a test 1',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
     ),
     new RecepieModel(
-      'A test recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+      'Test recipe 2',
+      'This is simply a test 2',
+      'https://p1.pxfuel.com/preview/548/595/580/gastronomy-food-dishes-eat.jpg'
     ),
   ];
+
+  selectedRecipe: RecepieModel;
 }
