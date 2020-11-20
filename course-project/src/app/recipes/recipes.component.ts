3676963bb9433d5eef1857fd54cb81fce980c510
Alex <alex.bejenaru93@gmail>
2020-11-20 11:19:51 +0100

    📦 NEW: Added property binding to the recipes component.

--- course-project/src/app/recipes/recipes.component.ts
+++ course-project/src/app/recipes/recipes.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { RecepieModel } from './recepie.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.scss']
+  styleUrls: ['./recipes.component.scss'],
 })
-export class RecipesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class RecipesComponent {
+  recipies: RecepieModel[] = [
+    new RecepieModel(
+      'A test recipe',
+      'This is simply a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    ),
+    new RecepieModel(
+      'A test recipe',
+      'This is simply a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    ),
+  ];
 }
