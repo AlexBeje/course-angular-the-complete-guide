12201223bb099740bc2d260090bbf2196ed1cabd
Alex <alex.bejenaru93@gmail>
2020-11-20 11:19:27 +0100

    📦 NEW: Added content to the recipe item component trough property binding (@Input)

--- course-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RecepieModel } from '../../recepie.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.scss']
+  styleUrls: ['./recipe-item.component.scss'],
 })
-export class RecipeItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class RecipeItemComponent {
+  @Input() recipie: RecepieModel;
+  constructor() {}
 }
