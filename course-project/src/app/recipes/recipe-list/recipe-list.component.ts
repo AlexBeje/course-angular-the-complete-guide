fee427227a815d4b21b933333b6ff511587d8f82
Alex <alex.bejenaru93@gmail>
2020-11-20 11:21:33 +0100

    📦 NEW: Moved the data to recipe item component trough property binding.

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RecepieModel } from '../recepie.model';
 
 @Component({
@@ -6,21 +6,6 @@ import { RecepieModel } from '../recepie.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss'],
 })
-export class RecipeListComponent implements OnInit {
-  recipies: RecepieModel[] = [
-    new RecepieModel(
-      'A test recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
-    ),
-    new RecepieModel(
-      'A test recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
-    ),
-  ];
-
-  constructor() {}
-
-  ngOnInit(): void {}
+export class RecipeListComponent {
+  @Input() recipies: RecepieModel;
 }
