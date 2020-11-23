c51cd033512bee859925f07ce7a873b7cabdb044
Alex <alex.bejenaru93@gmail>
2020-11-23 07:01:32 +0100

    📦 NEW: Added ingredients to the recipe detail.

--- course-project/src/app/recipes/services/recipe.service.ts
+++ course-project/src/app/recipes/services/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { IngredientModel } from 'src/app/shared/models/ingredient.model';
 
 import { RecipeModel } from '../models/recepie.model';
 
@@ -12,12 +13,18 @@ export class RecipeService {
     new RecipeModel(
       'Test recipe 1',
       'This is simply a test 1',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+      [
+        new IngredientModel('Meat', 1),
+        new IngredientModel('Salad', 1),
+        new IngredientModel('Tomatoes', 20),
+      ]
     ),
     new RecipeModel(
       'Test recipe 2',
       'This is simply a test 2',
-      'https://p1.pxfuel.com/preview/548/595/580/gastronomy-food-dishes-eat.jpg'
+      'https://p1.pxfuel.com/preview/548/595/580/gastronomy-food-dishes-eat.jpg',
+      [new IngredientModel('Rice', 1), new IngredientModel('Green Peas', 2)]
     ),
   ];
 
